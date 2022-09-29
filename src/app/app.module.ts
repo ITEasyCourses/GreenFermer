5796fc0266f63e0cab150fea48911d126069a886
Den <sokold3@gmail.com>
2022-09-29 16:16:19 +0300

    rased version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire/compat';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FeatherModule } from 'angular-feather';
+import { Camera, Github, Heart } from 'angular-feather/icons';
 
 import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+const icons = {
+  Camera,
+  Heart,
+  Github
+};
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,8 +22,10 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
+    FeatherModule.pick(icons)
   ],
+  exports: [FeatherModule],
   providers: [],
   bootstrap: [AppComponent]
 })
