84fc38af4a9944af6412dc6311386af215d5b135
unknown <bambinoogr@gmail.com>
2022-09-30 21:27:37 +0300

    create sort-select component

--- src/app/core/components/sort-select/sort-select.component.ts
+++ src/app/core/components/sort-select/sort-select.component.ts
@@ -1,18 +1,27 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output
+} from '@angular/core';
 import { FormControl } from '@angular/forms';
+
+import { SortTypeInterface } from '../../interfaces/sort-type.interface';
 
 @Component({
   selector: 'app-sort-select',
   templateUrl: './sort-select.component.html',
-  styleUrls: ['./sort-select.component.scss']
+  styleUrls: ['./sort-select.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SortSelectComponent {
-  @Input() public sortType: FormControl | any = new FormControl('');
-  @Input() public textDecoration!: string;
+  @Input() public sortType: FormControl = new FormControl('');
+  @Input() public placeholder!: string;
   @Output() public sortString: EventEmitter<string> =
     new EventEmitter<string>();
 
-  public mockSortTypes: any[] = [
+  public mockSortTypes: SortTypeInterface[] = [
     { value: 'fruits', viewValue: 'Фруткты' },
     { value: 'vegetables', viewValue: 'Овощи' },
     { value: 'all', viewValue: 'Всё' }
