4f5ad17ac150b26e063b5dd2497e52712c5fa5bb
Vogrant <vograntt@gmail.com>
2022-10-01 01:06:29 +0300

    apavlovikov/Header: waiting other components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponentModule } from './core/components/header-component/header-component.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,7 +15,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
+    HeaderComponentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
