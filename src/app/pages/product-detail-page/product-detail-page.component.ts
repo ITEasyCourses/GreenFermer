46f534fa3b8be12f4db4200d3492c33082741e40
sikey747 <Sikey747@gmail.com>
2022-10-01 06:01:56 +0300

    Create and connect Product-detail page

---
+++ src/app/pages/product-detail-page/product-detail-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-detail-page',
+  templateUrl: './product-detail-page.component.html',
+  styleUrls: ['./product-detail-page.component.scss']
+})
+export class ProductDetailPageComponent {
+  constructor() {}
+}
---
+++ src/app/pages/product-detail-page/product-detail-page.module.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ProductDetailPageComponent } from './product-detail-page.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ProductDetailPageComponent
+  }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, RouterModule.forChild(routes)]
+})
+export class ProductDetailPageModule {}
