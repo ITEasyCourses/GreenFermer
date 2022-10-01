3b465f78e57a448c3c576dfe949a3fa036c25786
Anton <vasiliii.balda@gmail.com>
2022-10-01 23:23:07 +0300

    aposmietniy/checkbox-component: in process

--- src/app/core/components/checkbox/checkbox.component.ts
+++ src/app/core/components/checkbox/checkbox.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-checkbox',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./checkbox.component.scss']
 })
 export class CheckboxComponent {
+  @Input() isChecked!: boolean;
   constructor() {}
 }
