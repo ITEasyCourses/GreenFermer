fff5d2f37d102f9367c452270052f9cb8ae1c0f9
Maksym <maksymkostiv32@gmail.com>
2022-10-01 12:56:34 +0300

    pre commit

--- src/app/core/components/search/search.module.ts
+++ src/app/core/components/search/search.module.ts
@@ -1,12 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FeatherModule } from 'angular-feather';
+import { Search } from 'angular-feather/icons';
 
 import { SearchComponent } from './search.component';
 
+const icons = {
+  Search
+};
 @NgModule({
   declarations: [SearchComponent],
   exports: [SearchComponent],
-  imports: [CommonModule, ReactiveFormsModule]
+  imports: [CommonModule, ReactiveFormsModule, FeatherModule.pick(icons)]
 })
 export class SearchModule {}
