3f684c0753e0ee7e59e028b1066155b39432de0d
KidinovNikita <birdfilm4@gmail.com>
2022-10-02 21:33:34 +0300

    akidinovlanguage

--- src/app/core/components/language/language.component.ts
+++ src/app/core/components/language/language.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-language',
   templateUrl: './language.component.html',
   styleUrls: ['./language.component.scss']
 })
-export class LanguageComponent {
-  public check = true;
+export class LanguageComponent implements OnInit {
+  @Input() isYellow = true;
+  @Input() check = true;
+  public color = 'yellow';
   constructor() {}
+  ngOnInit() {
+    this.color = this.isYellow ? 'yellow' : 'white';
+  }
 
   public button(): void {
     this.check = !this.check;
