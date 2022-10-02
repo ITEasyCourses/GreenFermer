70b041ae953b5b8420509b5e5ca524b56ba530b4
AndreyKlimmov <andrey.klimmov@gmail.com>
2022-10-02 09:14:54 +0300

    header select-component (temporary: import mock const, OnInit)

--- src/app/core/components/select/select.component.ts
+++ src/app/core/components/select/select.component.ts
@@ -1,13 +1,22 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output
+} from '@angular/core';
 
+import { CATEGORIES, LABEL_CATALOG } from '../../constants/mock-select';
 import { SortOption } from '../../interfaces/sort-option';
 
 @Component({
   selector: 'app-select',
   templateUrl: './select.component.html',
-  styleUrls: ['./select.component.scss']
+  styleUrls: ['./select.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SelectComponent {
+export class SelectComponent implements OnInit {
   @Input() public label = 'label';
   @Input() public options: SortOption[] = [];
 
@@ -15,6 +24,11 @@ export class SelectComponent {
     new EventEmitter<string>();
 
   constructor() {}
+
+  ngOnInit() {
+    this.label = LABEL_CATALOG;
+    this.options = CATEGORIES;
+  }
 
   public selectOption(option: string): void {
     this.selectedOption.emit(option);
