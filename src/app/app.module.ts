7c142cac9fbc85a04e975d812bfe2e7b1a5d4aef
Anton <vasiliii.balda@gmail.com>
2022-10-02 16:43:29 +0300

    aposmietniy/checkbox-component: created checkbox component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CheckboxModule } from './core/components/checkbox/checkbox.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,8 +14,7 @@ import { CheckboxModule } from './core/components/checkbox/checkbox.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    CheckboxModule
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core/components/checkbox/checkbox.component.ts
+++ src/app/core/components/checkbox/checkbox.component.ts
@@ -7,6 +7,4 @@ import { Component, Input } from '@angular/core';
 })
 export class CheckboxComponent {
   @Input() isChecked!: boolean;
-
-  constructor() {}
 }
