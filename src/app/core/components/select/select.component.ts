88fb08d5019a2d33f70b5d203f82dc4731177893
AndreyKlimmov <andrey.klimmov@gmail.com>
2022-10-04 22:20:08 +0300

    header select-component removed ngOnInit

--- src/app/core/components/select/select.component.ts
+++ src/app/core/components/select/select.component.ts
@@ -3,11 +3,9 @@ import {
   Component,
   EventEmitter,
   Input,
-  OnInit,
   Output
 } from '@angular/core';
 
-import { CATEGORIES, LABEL_CATALOG } from '../../constants/select.constants';
 import { SortOption } from '../../interfaces/sort-option';
 
 @Component({
@@ -16,17 +14,12 @@ import { SortOption } from '../../interfaces/sort-option';
   styleUrls: ['./select.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SelectComponent implements OnInit {
+export class SelectComponent {
   @Input() public label = 'label';
   @Input() public options: SortOption[] = [];
 
   @Output() public selectedOption: EventEmitter<string> =
     new EventEmitter<string>();
-
-  ngOnInit() {
-    this.label = LABEL_CATALOG;
-    this.options = CATEGORIES;
-  }
 
   public selectOption(option: string): void {
     this.selectedOption.emit(option);
