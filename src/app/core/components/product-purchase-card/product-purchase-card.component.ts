8cdb66fd31cde47fb5f2914c5869eacb00ead533
Anton <vasiliii.balda@gmail.com>
2022-10-05 00:42:41 +0300

    aposmietniy/product-purchase-card: precommit

--- src/app/core/components/product-purchase-card/product-purchase-card.component.ts
+++ src/app/core/components/product-purchase-card/product-purchase-card.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-product-purchase-card',
   templateUrl: './product-purchase-card.component.html',
-  styleUrls: ['./product-purchase-card.component.scss']
+  styleUrls: ['./product-purchase-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProductPurchaseCardComponent {
-  constructor() {}
+export class ProductPurchaseCardComponent implements OnInit {
+  @Input() productName!: string;
+  @Input() price!: string;
+  public counter = 1;
+  public totalPrice!: string;
+
+  public countPrice(operator: number): void {
+    const uah = +this.price.split('.')[0];
+    const cents = +this.price.split('.')[1];
+    if (this.price) {
+      if (operator === 0) {
+        if (this.counter !== 1) {
+          this.counter--;
+        } else this.counter = 1;
+      } else this.counter++;
+      this.totalPrice = `${uah}.${cents}`;
+    }
+  }
+
+  public ngOnInit(): void {
+    this.totalPrice = this.price;
+  }
 }
--- src/app/core/components/product-purchase-card/product-purchase-card.module.ts
+++ src/app/core/components/product-purchase-card/product-purchase-card.module.ts
@@ -1,11 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FeatherModule } from 'angular-feather';
+import { Minus, Plus, Trash } from 'angular-feather/icons';
 
 import { ProductPurchaseCardComponent } from './product-purchase-card.component';
+
+const icons = {
+  Plus,
+  Minus,
+  Trash
+};
 
 @NgModule({
   declarations: [ProductPurchaseCardComponent],
   exports: [ProductPurchaseCardComponent],
-  imports: [CommonModule]
+  imports: [CommonModule, FeatherModule.pick(icons)]
 })
 export class ProductPurchaseCardModule {}
