f7e998af1cf5d6d588160397e04ae6bf1412b75a
Vlad <artboy531@gmail.com>
2022-10-04 22:52:55 +0300

    fixed merge conflict

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ERoutes } from 'src/app/core/enums/routes';
-
 
 const routes: Routes = [];
-
-const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () =>
-      import('./pages/home-page/home-page.module').then((m) => m.HomePageModule)
-  },
-  {
-    path: 'product-detail',
-    loadChildren: () =>
-      import('./pages/product-detail-page/product-detail-page.module').then(
-        (m) => m.ProductDetailPageModule
-      )
-  },
-  {
-    path: '**',
-    redirectTo: ERoutes.HOME
-  }
-];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/core/interfaces/product-card.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface ProductCardInterface {
-  img: string;
-  title: string;
-}
