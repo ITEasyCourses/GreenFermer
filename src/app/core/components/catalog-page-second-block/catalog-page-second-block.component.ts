e050f2369a8a4145a71ba7e55d69f876827954a1
Evgeniy <joohnn048@gmail.com>
2022-10-05 23:37:54 +0300

    added Record interface in footer constants and delete superfluous code

--- src/app/core/components/catalog-page-second-block/catalog-page-second-block.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-catalog-page-second-block',
-  templateUrl: './catalog-page-second-block.component.html',
-  styleUrls: ['./catalog-page-second-block.component.scss']
-})
-export class CatalogPageSecondBlockComponent {}
