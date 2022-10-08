0962795e4b08b212442b1a1153f7eb7bbe5fc3f5
Vogrant <vograntt@gmail.com>
2022-10-08 18:42:39 +0300

    apavlovikov/order-card-component

---
+++ src/app/core/components/order-card-component/order-card-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-card-component',
+  templateUrl: './order-card-component.component.html',
+  styleUrls: ['./order-card-component.component.scss']
+})
+export class OrderCardComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/core/components/order-card-component/order-card-component.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OrderCardComponentComponent } from './order-card-component.component';
+
+
+
+@NgModule({
+  declarations: [
+    OrderCardComponentComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class OrderCardComponentModule { }
