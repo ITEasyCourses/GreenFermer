56756f502082e841aa92e1c83fdb254708b0c7f1
Vogrant <vograntt@gmail.com>
2022-10-10 02:52:45 +0300

    apavlovikov/complit heder page and create home page first block

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { IAuthenticationUser } from './core/interfaces/i-authentication-user';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title = 'green-fermer';
+  public sessionUser: IAuthenticationUser = {
+    isAuthentication: true,
+    avatar: '../assets/images/header-logo/images.jpg',
+    name: 'Сід',
+    id: 1
+  };
 }
