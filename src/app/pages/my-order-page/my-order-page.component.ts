c86ce1b36d3e885ee498f19dc9ed5387d22b1b2f
Anton <vasiliii.balda@gmail.com>
2022-10-10 14:14:12 +0300

    aposmietniy/my-order-page: precommit

--- src/app/pages/my-order-page/my-order-page.component.ts
+++ src/app/pages/my-order-page/my-order-page.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { SortOption } from '../../core/interfaces/sort-option';
+
 @Component({
   selector: 'app-my-order-page',
   templateUrl: './my-order-page.component.html',
@@ -7,5 +9,21 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MyOrderPageComponent {
+  public tittle!: string;
+  public options: SortOption[] = [
+    {
+      value: 'Status',
+      viewValue: 'status'
+    },
+    {
+      value: 'City',
+      viewValue: 'city'
+    },
+    {
+      value: 'Price',
+      viewValue: 'price'
+    }
+  ];
+
   constructor() {}
 }
--- src/app/pages/my-order-page/my-order-page.module.ts
+++ src/app/pages/my-order-page/my-order-page.module.ts
@@ -4,6 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { FeatherModule } from 'angular-feather';
 import { AlignJustify, Grid } from 'angular-feather/icons';
 
+import { SortSelectModule } from '../../core/components/sort-select/sort-select.module';
+
 import { MyOrderPageComponent } from './my-order-page.component';
 
 const icons = {
@@ -23,7 +25,8 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    FeatherModule.pick(icons)
+    FeatherModule.pick(icons),
+    SortSelectModule
   ]
 })
 export class MyOrderPageModule {}
