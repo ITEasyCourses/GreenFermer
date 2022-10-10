3f30592ebe794308f69998b3aab2bf915cdc24c7
AndreyKlimmov <andrey.klimmov@gmail.com>
2022-10-10 12:04:05 +0300

    aklimov/create-category-detail-page const routes: Routes

--- src/app/pages/category-page/category-page.module.ts
+++ src/app/pages/category-page/category-page.module.ts
@@ -2,12 +2,23 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { CategoryDetailPageComponent } from '../category-detail-page/category-detail-page.component';
+
 import { CategoryPageComponent } from './category-page.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: CategoryPageComponent
+    children: [
+      {
+        path: '',
+        component: CategoryPageComponent
+      },
+      {
+        path: 'detail',
+        component: CategoryDetailPageComponent
+      }
+    ]
   }
 ];
 
