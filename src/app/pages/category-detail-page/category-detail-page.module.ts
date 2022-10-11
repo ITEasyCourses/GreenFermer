8f4a66938462c544ed711b7b2a8d39ac6c7985e6
vansa-p <vansa.pashka@gmail.com>
2022-10-11 19:43:15 +0300

    write kod in categoru-detail-page

--- src/app/pages/category-detail-page/category-detail-page.module.ts
+++ src/app/pages/category-detail-page/category-detail-page.module.ts
@@ -1,10 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { BreadcrumbsModule } from '../../core/components/breadcrumbs/breadcrumbs.module';
+import { ProductCardModule } from '../../core/components/product-card/product-card.module';
+
 import { CategoryDetailPageComponent } from './category-detail-page.component';
 
 @NgModule({
   declarations: [CategoryDetailPageComponent],
-  imports: [CommonModule]
+  exports: [CategoryDetailPageComponent],
+  imports: [CommonModule, BreadcrumbsModule, ProductCardModule]
 })
 export class CategoryDetailPageModule {}
