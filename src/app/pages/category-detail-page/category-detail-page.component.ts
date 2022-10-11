72c92f1bb74b65f22a1d308ce9e031c22b3a4dbb
AndreyKlimmov <andrey.klimmov@gmail.com>
2022-10-11 17:49:30 +0300

    aklimov/create-category-detail-page

--- src/app/pages/category-detail-page/category-detail-page.component.ts
+++ src/app/pages/category-detail-page/category-detail-page.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-category-detail-page',
   templateUrl: './category-detail-page.component.html',
-  styleUrls: ['./category-detail-page.component.scss']
+  styleUrls: ['./category-detail-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CategoryDetailPageComponent {}
