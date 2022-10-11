ba209e9b0483e7b290f7bea5c520e23449c437a5
Vlodymyr <pedko.vladimir3535@gmail.com>
2022-10-11 16:45:30 +0300

    pre commit

--- src/app/core/components/purchase-total/purchase-total.component.ts
+++ src/app/core/components/purchase-total/purchase-total.component.ts
@@ -9,36 +9,28 @@ export class PurchaseTotalComponent implements OnInit {
   @Input() allProductPrice!: number;
   @Input() package!: number;
   @Input() delivery!: number;
-  @Input() totalProduct = 1;
+  @Input() totalProduct!: number;
   public allPrice!: number;
   public totalProductText!: string;
-
-  constructor() {}
 
   ngOnInit(): void {
     this.allPrice =
       this.allProductPrice + (this.package || 0) + (this.delivery || 0);
-  }
 
-  public product(): void {
     if (
       this.totalProduct === 1 ||
       (this.totalProduct > 20 && (this.totalProduct - 1) % 10 === 0)
     ) {
-      this.totalProductText = 'товар';
-    }
+      this.totalProductText = ' товар';
+    } else if (
+      this.totalProduct <= 4 ||
+      (this.totalProduct > 21 && (this.totalProduct - 2) % 10 === 0) ||
+      (this.totalProduct > 21 && (this.totalProduct - 3) % 10 === 0) ||
+      (this.totalProduct > 21 && (this.totalProduct - 4) % 10 === 0)
+    ) {
+      this.totalProductText = ' товара';
+    } else if (this.totalProduct <= 10) {
+      this.totalProductText = ' товарів';
+    } else this.totalProductText = ' товарів';
   }
 }
-
-// totalProductText = totalProduct =;
-//     if (totalProduct === 1 || totalProduct > 20 && (totalProduct - 1) %10 === 0) {
-//         (`${totalProduct} товар`)
-//     } else if (totalProduct <= 4 || totalProduct > 21 && (totalProduct - 2) %10 === 0 ||
-//       totalProduct > 21 && (totalProduct -3) %10 === 0 || totalProduct > 21 && (totalProduct - 4) %10 === 0) {
-//      (`${totalProduct} товара`)
-//     }
-//     else if (totalProduct <= 10) {
-//         ('${totalProduct} товаров')
-//     }
-//     else
-//         (`${totalProduct} товаров`)
