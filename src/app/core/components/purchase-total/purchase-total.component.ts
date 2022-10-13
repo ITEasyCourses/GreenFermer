296604a5fe55b49401d73cb1c3f6986101adb54f
Vlodymyr <pedko.vladimir3535@gmail.com>
2022-10-13 21:17:31 +0300

    purchase-total fix bag

--- src/app/core/components/purchase-total/purchase-total.component.ts
+++ src/app/core/components/purchase-total/purchase-total.component.ts
@@ -1,21 +1,48 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output
+} from '@angular/core';
 
 @Component({
   selector: 'app-purchase-total',
   templateUrl: './purchase-total.component.html',
-  styleUrls: ['./purchase-total.component.scss']
+  styleUrls: ['./purchase-total.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PurchaseTotalComponent implements OnInit {
   @Input() allProductPrice!: number;
   @Input() package!: number;
   @Input() delivery!: number;
   @Input() totalProduct!: number;
+  @Output() addOrder: EventEmitter<any> = new EventEmitter<any>();
+  @Output() cancelOrder: EventEmitter<any> = new EventEmitter<any>();
+
   public allPrice!: number;
   public totalProductText!: string;
 
   ngOnInit(): void {
+    this.allSum();
+    this.wordProduct();
+  }
+
+  public confirmOrder(): void {
+    this.addOrder.emit();
+  }
+
+  public editOrder(): void {
+    this.cancelOrder.emit();
+  }
+
+  private allSum(): void {
     this.allPrice =
       this.allProductPrice + (this.package || 0) + (this.delivery || 0);
+  }
+
+  private wordProduct(): void {
     if (
       this.totalProduct === 1 ||
       (this.totalProduct > 20 && (this.totalProduct - 1) % 10 === 0)
@@ -32,8 +59,4 @@ export class PurchaseTotalComponent implements OnInit {
       this.totalProductText = ' товарів';
     } else this.totalProductText = ' товарів';
   }
-
-  public confirmOrder(): void {}
-
-  public editOrder(): void {}
 }
