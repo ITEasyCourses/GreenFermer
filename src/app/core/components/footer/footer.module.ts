420b63f2731b03e5d9d554bccc4eede39efb04ad
Evgeniy <joohnn048@gmail.com>
2022-10-14 19:49:59 +0300

    Added navigation to the footer

--- src/app/core/components/footer/footer.module.ts
+++ src/app/core/components/footer/footer.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterLinkWithHref } from '@angular/router';
 
 import { FooterComponent } from './footer.component';
 
 @NgModule({
   declarations: [FooterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLinkWithHref],
   exports: [FooterComponent]
 })
 export class FooterModule {}
--- src/app/pages/home-page/home-page.module.ts
+++ src/app/pages/home-page/home-page.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ERoutes } from 'src/app/core/enums/routes';
 
+import { HomePageComponent } from './home-page.component';
 import { ProductCardModule } from '../../core/components/product-card/product-card.module';
 import { SearchModule } from '../../core/components/search/search.module';
 import { SliderModule } from '../../core/components/slider/slider.module';
 
-import { HomePageComponent } from './home-page.component';
 
 const routes: Routes = [
   {
