7aa947eafc86effab42d7c815099899df158146b
Maksym <maksymkostiv32@gmail.com>
2022-10-15 13:48:54 +0300

    Complete task

--- src/app/pages/category-page/category-page.component.ts
+++ src/app/pages/category-page/category-page.component.ts
@@ -1,12 +1,7 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import { ProductCardConstants } from '../../core/constants/product-card.constants';
-import {
-  CATEGORIES,
-  LABEL_SELECT
-} from '../../core/constants/select.constants';
 import { IProductCard } from '../../core/interfaces/i-product-card';
-import { SortOption } from '../../core/interfaces/sort-option';
 
 @Component({
   selector: 'app-category-page',
@@ -14,13 +9,6 @@ import { SortOption } from '../../core/interfaces/sort-option';
   styleUrls: ['./category-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CategoryPageComponent implements OnInit {
+export class CategoryPageComponent {
   allProduct: IProductCard[] = ProductCardConstants;
-  public labelForSelectKatalog!: string;
-  public mocListForOptionSelect!: SortOption[];
-
-  public ngOnInit(): void {
-    this.labelForSelectKatalog = LABEL_SELECT.CATALOG;
-    this.mocListForOptionSelect = CATEGORIES;
-  }
 }
