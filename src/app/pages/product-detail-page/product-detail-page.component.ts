8e723d88625d5cf22a4c5d64d2cdab150646cac7
Vogrant <vograntt@gmail.com>
2022-10-18 16:31:04 +0300

    apavlovikova/bottom-section-prod-det-page

--- src/app/pages/product-detail-page/product-detail-page.component.ts
+++ src/app/pages/product-detail-page/product-detail-page.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import { PRODUCT_DESCRIPTION } from '../../core/constants/product-detail.constants';
+import { IProductCard } from '../../core/interfaces/i-product-card';
+import { SortOption } from '../../core/interfaces/sort-option';
 
 @Component({
   selector: 'app-product-detail-page',
@@ -10,4 +12,34 @@ import { PRODUCT_DESCRIPTION } from '../../core/constants/product-detail.constan
 })
 export class ProductDetailPageComponent {
   public description = PRODUCT_DESCRIPTION;
+
+  public cards: IProductCard[] = [
+    {
+      name: 'Полуниця',
+      price: 123,
+      image: ''
+    },
+    {
+      name: 'Малина',
+      price: 123,
+      image: ''
+    },
+    {
+      name: 'Лохина',
+      price: 123,
+      image: ''
+    },
+    {
+      name: 'Малина',
+      price: 123,
+      image: ''
+    },
+    {
+      name: 'Малина',
+      price: 123,
+      image: ''
+    }
+  ];
+
+  public mockSortTypes!: SortOption[];
 }
--- src/app/pages/product-detail-page/product-detail-page.module.ts
+++ src/app/pages/product-detail-page/product-detail-page.module.ts
@@ -2,6 +2,9 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ProductCardModule } from '../../core/components/product-card/product-card.module';
+import { SortSelectModule } from '../../core/components/sort-select/sort-select.module';
+
 import { ProductDetailPageComponent } from './product-detail-page.component';
 
 const routes: Routes = [
@@ -13,6 +16,11 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [ProductDetailPageComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)]
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    ProductCardModule,
+    SortSelectModule
+  ]
 })
 export class ProductDetailPageModule {}
