393f1fe0f05ee6ef842c38655bf7cd4830d79f02
DemhukVit <demchukvitalii.kr@gmail.com>
2022-10-19 23:28:43 +0200

    fix again

--- src/app/pages/checkout-page/checkout-page.component.ts
+++ src/app/pages/checkout-page/checkout-page.component.ts
@@ -7,14 +7,8 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CheckoutPageComponent {
-  public state: boolean | undefined;
-  private number: number | undefined;
+  public state = false;
   public changeState(number: number): void {
-    this.number = number;
-    if (this.number === 1) {
-      this.state = false;
-    } else if (this.number === 2) {
-      this.state = true;
-    }
+    number === 1 ? (this.state = false) : (this.state = true);
   }
 }
