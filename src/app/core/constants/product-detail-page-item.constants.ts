638bcc94c8dcec6b0e17ae70994d862613caf03d
Evgeniy <joohnn048@gmail.com>
2022-10-28 17:49:33 +0300

    Added constants

---
+++ src/app/core/constants/product-detail-page-item.constants.ts
@@ -0,0 +1,27 @@
+export const PRODUCT_ITEM = [
+  {
+    name: 'Полуниця',
+    price: '123',
+    image: ''
+  },
+  {
+    name: 'Малина',
+    price: '123',
+    image: ''
+  },
+  {
+    name: 'Лохина',
+    price: '123',
+    image: ''
+  },
+  {
+    name: 'Малина',
+    price: '123',
+    image: ''
+  },
+  {
+    name: 'Малина',
+    price: '123',
+    image: ''
+  }
+]
--- src/app/pages/product-detail-page/product-detail-page.component.ts
+++ src/app/pages/product-detail-page/product-detail-page.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import { feedbackConstants } from '../../core/constants/feedback.constants';
+import { PRODUCT_ITEM } from '../../core/constants/product-detail-page-item.constants';
 import { PRODUCT_DESCRIPTION } from '../../core/constants/product-detail.constants';
 import { Feedback } from '../../core/interfaces/feedback-interface';
 import { IProductCard } from '../../core/interfaces/i-product-card';
@@ -15,33 +16,7 @@ import { SortOption } from '../../core/interfaces/sort-option';
 export class ProductDetailPageComponent {
   public description = PRODUCT_DESCRIPTION;
 
-  public cards: IProductCard[] = [
-    {
-      name: 'Полуниця',
-      price: '123',
-      image: ''
-    },
-    {
-      name: 'Малина',
-      price: '123',
-      image: ''
-    },
-    {
-      name: 'Лохина',
-      price: '123',
-      image: ''
-    },
-    {
-      name: 'Малина',
-      price: '123',
-      image: ''
-    },
-    {
-      name: 'Малина',
-      price: '123',
-      image: ''
-    }
-  ];
+  public cards: IProductCard[] = PRODUCT_ITEM;
 
   public mockSortTypes!: SortOption[];
   public arrFeedback: Feedback[] = feedbackConstants;
