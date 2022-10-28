eaa7f59a4ba0cb9e61ed14f019260c9dcfd0300a
unknown <bambinoogr@gmail.com>
2022-10-28 14:01:38 +0300

    fix select-delivery

--- src/app/core/components/delivery-payment/delivery-payment.component.ts
+++ src/app/core/components/delivery-payment/delivery-payment.component.ts
@@ -74,7 +74,6 @@ export class DeliveryPaymentComponent {
     if (this.deliveryFB) {
       this.deliveryFB.patchValue({ payment: this.paymentType });
       if (this.deliveryFB.valid) {
-        console.log(this.deliveryFB.value);
         this.deliveryObj.emit(this.deliveryFB.value);
       }
     }
