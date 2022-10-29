f097f379e0c559424872b8debf86a07a1059e586
17-09-1797 <jake_connor921@mail.ru>
2022-10-29 16:18:26 +0300

    dlagunov/create_the_second_block_on_the_Checkout_page: fix

--- src/app/pages/checkout-page/checkout-page.component.ts
+++ src/app/pages/checkout-page/checkout-page.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 import { SelectOption } from '../../core/interfaces/delivery-type';
+import { IProductCardBucket } from '../../core/interfaces/product-card-bucket.interface';
 
 @Component({
   selector: 'app-checkout-page',
@@ -8,10 +9,15 @@ import { SelectOption } from '../../core/interfaces/delivery-type';
   styleUrls: ['./checkout-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CheckoutPageComponent {
+export class CheckoutPageComponent implements OnInit {
   public state = false;
   public label!: string;
-  public totalPrice!: string;
+  public totalPrice = 0;
+  public data: IProductCardBucket[] = [];
+
+  public ngOnInit(): void {
+    this.getTotalPrice();
+  }
 
   public changeState(state: boolean): void {
     this.state = state;
@@ -19,5 +25,19 @@ export class CheckoutPageComponent {
 
   public selectedPackage(selectedOption: SelectOption): void {
     this.label = selectedOption.viewValue;
+  }
+
+  public onUpdateProductCard(card: IProductCardBucket): void {
+    this.data = this.data.map((el) => {
+      if (card.id === el.id) {
+        el = Object.assign({}, card);
+      }
+      return el;
+    });
+    this.getTotalPrice();
+  }
+
+  private getTotalPrice(): void {
+    this.totalPrice = this.data.reduce((acc, el) => (acc += +el.totalPrice), 0);
   }
 }
