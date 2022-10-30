70a4523e6380416f44fc35e9143879407ea96d30
Vogrant <vograntt@gmail.com>
2022-10-30 15:49:35 +0200

    apavlovikov/product-card-bugs/fix

--- src/app/pages/category-detail-page/category-detail-page.component.ts
+++ src/app/pages/category-detail-page/category-detail-page.component.ts
@@ -17,7 +17,7 @@ export class CategoryDetailPageComponent {
   @Input() products: IProductCard[] = CategoryDetailConst;
 
   public mockSortTypes: SortOption[] = sortMapOptions;
-  public isCategoryPage?: boolean;
+  public isCategoryPage!: boolean;
 
   constructor(private router: Router) {}
 
