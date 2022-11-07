4cc1ffa1e6b46fcb04ea3cfbc93357e380de610e
unknown <bambinoogr@gmail.com>
2022-11-07 19:58:48 +0200

    complete breadcrumbs on category-detail

--- src/app/core/components/product-card/product-card.component.ts
+++ src/app/core/components/product-card/product-card.component.ts
@@ -6,8 +6,11 @@ import {
   OnInit,
   Output
 } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
+import { ERoutes } from '../../enums/routes';
 import { IProductCard } from '../../interfaces/i-product-card';
+
 
 @Component({
   selector: 'app-product-card',
@@ -22,6 +25,7 @@ export class ProductCardComponent implements OnInit {
   @Input() card!: IProductCard;
   public heart = false;
   public img!: string | undefined;
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.findImg();
@@ -33,6 +37,17 @@ export class ProductCardComponent implements OnInit {
 
   public like(): void {
     this.heart = !this.heart;
+  }
+
+  public goToCategoryDetails(): void {
+    const categoryTypeId =
+      this.activatedRoute.snapshot.params['categoryTypeId'];
+
+    this.router.navigate([
+      ERoutes.CATALOG_PAGE,
+      this.card.categoryId || categoryTypeId,
+      this.card.typeId
+    ]);
   }
 
   private findImg(): void {
--- src/app/pages/category-detail-page/category-detail-page.component.ts
+++ src/app/pages/category-detail-page/category-detail-page.component.ts
@@ -1,13 +1,17 @@
 import {
   ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
   Input,
+  OnInit,
   TrackByFunction
 } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { sortMapOptions } from '../../core/constants/sort-map-options';
 import { IProductCard } from '../../core/interfaces/i-product-card';
 import { SortOption } from '../../core/interfaces/sort-option';
+import { CategoryService } from '../../core/services/category.service';
 
 @Component({
   selector: 'app-category-detail-page',
@@ -15,10 +19,31 @@ import { SortOption } from '../../core/interfaces/sort-option';
   styleUrls: ['./category-detail-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CategoryDetailPageComponent {
+export class CategoryDetailPageComponent implements OnInit {
   @Input() products!: IProductCard[];
 
   public mockSortTypes: SortOption[] = sortMapOptions;
 
+  constructor(
+    private categoryService: CategoryService,
+    private activatedRoute: ActivatedRoute,
+    private cdr: ChangeDetectorRef
+  ) {}
+
   public trackByFn: TrackByFunction<IProductCard> = (index, item) => item.id;
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  private getProducts(): void {
+    const categoryId = this.activatedRoute.snapshot.params['categoryTypeId'];
+    const typeId = this.activatedRoute.snapshot.params['categoryId'];
+    this.categoryService
+      .getCategoryDetail(categoryId, typeId)
+      .subscribe((data) => {
+        this.products = data;
+        this.cdr.detectChanges();
+      });
+  }
 }
